6ea76ba4677aa60160a858e6dec273db8e1847d7
efuentealbaCH <efuentealba.git@gmail.com>
2022-12-27 19:07:12 -0300

    feat:vista de graficos con datos estaticos

--- resources/views/admin/charts/graficos.blade.php
+++ resources/views/admin/charts/graficos.blade.php
@@ -1,12 +1,43 @@
 @extends('admin.home')
 
 @section('contenido')
-<div class="card">
-    <div class="card-header">
-      <h4>Line Chart</h4>
-    </div>
-    <div class="card-body">
-      <canvas id="myChart"></canvas>
-    </div>
-  </div>
+    <section class="container">
+        <div class="section-body">
+            <div class="row">
+                <div class="col-12 col-md-6 col-lg-6">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4>Iniciativas</h4>
+                        </div>
+                        <div class="card-body">
+                            <canvas id="iniciativasChart"></canvas>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12 col-md-6 col-lg-6">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4>Organizaciones</h4>
+                        </div>
+                        <div class="card-body">
+                            <canvas id="organizacionesChart"></canvas>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12 col-md-6 col-lg-6">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4>Inversión</h4>
+                        </div>
+                        <div class="card-body">
+                            <canvas id="inverisionesChart"></canvas>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 @endsection
